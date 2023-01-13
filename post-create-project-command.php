10bc5d50e428f4ca5cfae87edc3975fa6fc7dea6
GhianCo <ghiancorx@gmail.com>
2023-01-13 15:53:50 -0500

    cambios

---
+++ post-create-project-command.php
@@ -0,0 +1,16 @@
+<?php
+
+echo <<<EOF
+
+\033[32mProyecto creado con exito!\033[37m
+
+Ejecuta los siguientes comandass:
+
+$ cd [path-project]
+
+\033[33mRecuerda!\033[37m
+Configura tus accesos de la base de datos en: '.env'.
+
+EOF;
+
+unlink('post-create-project-command.php');

8ce0a122181c25d96c5e06111a1e1963bd569d82
GhianCo <ghiancorx@gmail.com>
2023-10-26 16:58:56 -0500

    cambios

--- post-create-project-command.php
+++ post-create-project-command.php
@@ -13,4 +13,31 @@ Configura tus accesos de la base de datos en: '.env'.
 
 EOF;
 
+function replace_file($path, $string, $replace)
+{
+    set_time_limit(0);
+
+    if (is_file($path) === true)
+    {
+        $file = fopen($path, 'r');
+        $temp = tempnam('./', 'tmp');
+
+        if (is_resource($file) === true)
+        {
+            while (feof($file) === false)
+            {
+                file_put_contents($temp, str_replace($string, $replace, fgets($file)), FILE_APPEND);
+            }
+
+            fclose($file);
+        }
+
+        unlink($path);
+    }
+
+    return rename($temp, $path);
+}
+
+replace_file('.env', 'sophy-framework', 'pepe');
+
 unlink('post-create-project-command.php');
